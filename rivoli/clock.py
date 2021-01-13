ac7367a7b1382602dfb071c72a35dda594975884
remidbs <remi.delbouys@laposte.net>
2021-01-13 09:20:56 +0100

    different seed for different counters

--- rivoli/clock.py
+++ rivoli/clock.py
@@ -15,7 +15,6 @@ SCHED = BlockingScheduler()
 
 
 def _post_exceptions_to_slack(func: Callable, args: List[Any]):
-    random.seed(date.today().day)  # ensure posted tweet is the one that was inspected
     try:
         func(*args)
         success_message = f'Succesfully executed func {func} with args {args}'
@@ -27,21 +26,25 @@ def _post_exceptions_to_slack(func: Callable, args: List[Any]):
 
 @SCHED.scheduled_job('cron', hour=7, minute=0, timezone=pytz.timezone('Europe/Paris'))
 def rivoli_post_to_slack():
+    random.seed(1)
     _post_exceptions_to_slack(fetch_data_and_post_tweet_to_slack, [CounterName.RIVOLI])
 
 
 @SCHED.scheduled_job('cron', hour=7, minute=0, timezone=pytz.timezone('Europe/Paris'))
 def sebastopol_post_to_slack():
+    random.seed(2)
     _post_exceptions_to_slack(fetch_data_and_post_tweet_to_slack, [CounterName.SEBASTOPOL])
 
 
 @SCHED.scheduled_job('cron', hour=9, minute=30, timezone=pytz.timezone('Europe/Paris'))
 def rivoli_tweet():
+    random.seed(1)
     _post_exceptions_to_slack(fetch_data_and_publish_tweet, [CounterName.RIVOLI])
 
 
 @SCHED.scheduled_job('cron', hour=9, minute=30, timezone=pytz.timezone('Europe/Paris'))
 def sebastopol_tweet():
+    random.seed(2)
     _post_exceptions_to_slack(fetch_data_and_publish_tweet, [CounterName.SEBASTOPOL])
 
 
